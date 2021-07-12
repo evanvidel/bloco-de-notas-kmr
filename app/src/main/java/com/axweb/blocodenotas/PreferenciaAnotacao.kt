9dd210f7be62139d8cca4c8f9c3c13887302dd8d
evanvidel <evanvidel@gmail.com>
2021-07-12 12:06:30 -0300

    save and retrieve note

---
+++ app/src/main/java/com/axweb/blocodenotas/PreferenciaAnotacao.kt
@@ -0,0 +1,26 @@
+package com.axweb.blocodenotas
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenciaAnotacao(private val context: Context) {
+
+    private val ARQUIVO = "anotacao.preferencia"
+    private val CHAVE = "nome"
+    private val preferences: SharedPreferences
+    private val editor: SharedPreferences.Editor
+
+    fun saveNote(anotacao: String?){
+        editor.putString(CHAVE,anotacao)
+        editor.commit()
+    }
+
+    fun retrieveNote(): String?{
+        return preferences.getString(CHAVE,"")
+    }
+
+    init {
+        preferences = context.getSharedPreferences(ARQUIVO,0)
+        editor = preferences.edit()
+    }
+}
